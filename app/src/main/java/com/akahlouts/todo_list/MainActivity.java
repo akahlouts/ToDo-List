62903e3162e2569545792d68d9889d1309df5df0
akahlouts <akahlouts@gmail.com>
2020-11-19 23:31:00 +0200

    The second UI is done

--- app/src/main/java/com/akahlouts/todo_list/MainActivity.java
+++ app/src/main/java/com/akahlouts/todo_list/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/akahlouts/todo_list/SignupActivity.java
+++ app/src/main/java/com/akahlouts/todo_list/SignupActivity.java
@@ -2,13 +2,17 @@ package com.akahlouts.todo_list;
 
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void Create(View view) {
     }
 }
