06c16f1e2e8900bd64ae6acf9574b80c5ad36fe9
akahlouts <akahlouts@gmail.com>
2020-12-01 11:20:10 +0200

    The Third UI is done

--- app/src/main/java/com/akahlouts/todo_list/LoginActivity.java
+++ app/src/main/java/com/akahlouts/todo_list/LoginActivity.java
@@ -1,31 +1,30 @@
 package com.akahlouts.todo_list;
 
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    TextView next;
+public class LoginActivity extends AppCompatActivity {
+
+    TextView textView_create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-
-        next = findViewById(R.id.next);
-        next.setOnClickListener(new View.OnClickListener() {
+        textView_create = findViewById(R.id.textView_create);
+        textView_create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
                 startActivity(intent);
             }
         });
-
-
     }
+
 }
--- app/src/main/java/com/akahlouts/todo_list/SignupActivity.java
+++ app/src/main/java/com/akahlouts/todo_list/SignupActivity.java
@@ -1,17 +1,32 @@
 package com.akahlouts.todo_list;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
+
+    TextView textView_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        textView_login = findViewById(R.id.textView_login);
+        textView_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 
     public void Create(View view) {
     }
